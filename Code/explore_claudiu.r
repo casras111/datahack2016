647098b9c1a3a8ae236954dd42ccbe32c2418d10
claudiu <claudiu.schiller@gmail.com>
2016-09-21 22:40:07 +0300

    fix NA in load_dat, add returns for all indexes, new explore file

---
+++ Code/explore_claudiu.r
@@ -0,0 +1,12 @@
+#Explore features and predicted indexes
+
+library(quantmod)
+library(ggplot2)
+library(reshape2)
+
+load("DataWork/VixDat.Rdata")
+
+plotdat <- as.data.frame(cumprod(VixDat[-1,c("SPXRet","PUTRet","VXORet")]/100+1))
+plotdat$Date <- index(VixDat[-1,])
+plotdat <- melt(plotdat,id="Date",value.name = "Price")
+ggplot(plotdat,aes(x=Date,y=Price,colour=variable))+geom_line()
