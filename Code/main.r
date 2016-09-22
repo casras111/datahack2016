518bd2085b36cbe35d1f31dfa4fae33b8fd8e9bb
claudiu <claudiu.schiller@gmail.com>
2016-09-22 08:41:18 +0300

    added main, moved Classifier to code dir

---
+++ Code/main.r
@@ -0,0 +1,10 @@
+#Main module sourcing all parts of the project
+
+#Feature create scripts from Claudiu and Amit
+source("Code/feature_create_claudiu.r")
+
+#Classifiers from all, modify as needed to use what you're testing now
+source("Code/Classification.R") #Oz current one
+
+#Placeholder for Dror's module
+#source("Code/Trading.r")
