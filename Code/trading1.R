c5719fd856f737fe2d99e1f83cd6f634d3d227dc
dror101 <essdror@hotmail.com>
2016-09-22 14:45:39 +0300

    addedtrading file

--- Code/trading1.R
+++ Code/trading1.R
@@ -1,18 +1,15 @@
 library(zoo)
 library(xts)
+library(PerformanceAnalytics)
 load("DataWork/VixDat.Rdata")
 
 Hefresh = 1+(as.numeric(VixDat[-1,"PUT"]) - as.numeric(VixDat[-nrow(VixDat),"PUT"]))/VixDat[-nrow(VixDat),"PUT"]
 aggRet<-cumprod(Hefresh)
-x<-ifelse(runif(nrow(aggRet),-1,1)<0.5,-1,1)
-hef2<-1+cumprod(Hefresh*x)
-dat<-merge(aggRet,x)
+x<-ifelse(runif(nrow(VixDat$PUTRet[-1]),-1,1)<0.5,-1,1)
+#hef2<-1+cumprod(Hefresh*x)
 plot(hef2)
+hef2<-cumprod((VixDat$PUTRet[-1])*x)+1
 
-
-
-
-
- 
+plot 
 
 
