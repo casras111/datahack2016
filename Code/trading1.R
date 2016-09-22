f90e0a97c72373e9c84fa3bcd2c941b757c67c69
AlexaOsip <alexa_os@walla.co.ilgit config --global alexandra>
2016-09-22 11:26:44 +0300

    Dror

---
+++ Code/trading1.R
@@ -0,0 +1,19 @@
+library(zoo)
+library(xts)
+
+load("C:/Users/essdr/Desktop/data science/financial math/VixDat.Rdata")
+Hefresh = 1+(as.numeric(VixDat[-1,"PUT"]) - as.numeric(VixDat[-nrow(VixDat),"PUT"]))/VixDat[-nrow(VixDat),"PUT"]
+aggRet<-cumprod(Hefresh)
+x<-ifelse(runif(nrow(aggRet),-1,1)<0.5,-1,1)
+hef2<-1+cumprod(Hefresh*x)
+dat<-merge(aggRet,x)
+plot.zoo(aggRet)
+plot(hef2)
+
+
+
+
+
+ 
+
+
