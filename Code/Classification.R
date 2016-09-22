c2f9926066cd4384b8889afb0665f3fdd261d26d
AlexaOsip <alexa_os@walla.co.ilgit config --global alexandra>
2016-09-22 17:25:45 +0300

    1sr classifier by date

--- Code/Classification.R
+++ Code/Classification.R
@@ -1,7 +1,23 @@
-Alexa<-function(){
+Alexa<-function(Day2class){
+#Day2class<- "2013-11-27"
 load("DataWork/VixDat.RData")
-classiffication <- ifelse(VixDat[,"PUTRet"]<0,0,1)
-VixDat<-cbind(VixDat,classiffication)
+
+Day2class<-paste0("1988-06-01/",Day2class) #start Date:enDate
+VixDat<-VixDat[Day2class]
+Last<-VixDat[nrow(VixDat),]#t(data.frame(as.numeric(VixDat[nrow(VixDat),]))) #number of rows take the last one
+colnames(Last)<-names(VixDat)
+rownames(Last)<-1:nrow(Last)
+
+classiffication <-as.numeric( ifelse(VixDat[,"PUTRet"]<0,0,1))
+classiffication<-classiffication[-1] #we dont want the first line
+Date<-index(VixDat)[-nrow(VixDat)]
+classiffication<-xts(classiffication,Date)
+VixDat<-VixDat[-nrow(VixDat),] #we want to gess the last line so we delete this line
+Date<-index(VixDat)
+#new column:
+VixDat$classiffication <-classiffication
+#classiffication<-xts(classiffication,Date)
+#VixDat<-merge.xts(VixDat,classiffication)
 #sum(is.na(VixDat[,"VXO"]))
 DelRows<-which(is.na(VixDat[,"VXO"]))
 VixDat<-VixDat[-DelRows,]
@@ -9,34 +25,42 @@ VixDat<-VixDat[-DelRows,]
 
 Top.Cut.Off<-0
 for (cutoff in seq(0,1,0.01)){
-probit<-glm(formula = PUTRet.1 ~VXO+SPX, family =binomial(link="probit"),data = VixDat)
+probit<-glm(formula = classiffication ~VXO+SPX, family =binomial(link="probit"),data = VixDat)
 #summary(probit)
-#a<-mean(VixDat[-1,"PUTRet.1"])
+#a<-mean(VixDat[-1,"classiffication"])
 
 Prediction <- ifelse(probit$fitted.values<cutoff,0,1)
 #sum(is.na(VixDat[,"VXO"]))
 #sum(is.na(VixDat[,"SPX"]))
-Top.Cut.Off<-c(Top.Cut.Off,mean(1-abs(Prediction-VixDat[-1,"PUTRet.1"])))
+Top.Cut.Off<-c(Top.Cut.Off,mean(1-abs(Prediction-VixDat[,"classiffication"])))
 }
 
 plot(seq(0,1,0.01),Top.Cut.Off[-1])
 BestCutof<-c(seq(0,1,0.01))[which(Top.Cut.Off[-1]==max(Top.Cut.Off))]
 
-probit<-glm(formula = PUTRet.1 ~VXO+SPX, family =binomial(link="probit"),data = VixDat)
+probit<-glm(formula = classiffication ~VXO+SPX, family =binomial(link="probit"),data = VixDat)
 #summary(probit)
-#a<-mean(VixDat[-1,"PUTRet.1"])
+#a<-mean(VixDat[-1,"classiffication"])
+PredicTest<-predict(probit, Last[,c("VXO","SPX")], type="response") 
 
-Prediction <- ifelse(probit$fitted.values<BestCutof,0,1)
+BuySell<-ifelse(PredicTest<BestCutof,0,1)
+
+
+
+
+#Prediction <- ifelse(probit$fitted.values<BestCutof,0,1)
 #sum(is.na(VixDat[,"VXO"]))
 #sum(is.na(VixDat[,"SPX"]))
-Top.Cut.Off<-c(Top.Cut.Off,mean(1-abs(Prediction-VixDat[-1,"PUTRet.1"])))
-Date<-index(VixDat)
-
-Prediction<-as.numeric(c("NA",Prediction))
-Prediction<-xts(Prediction,Date)
-VixDat<-merge.xts(VixDat,Prediction)
+#Top.Cut.Off<-c(Top.Cut.Off,mean(1-abs(Prediction-VixDat[-1,"classiffication"])))
 
 
-mean(1-abs(VixDat[-1,"Prediction"]-VixDat[-1,"PUTRet.1"]))
-save(Prediction,file = "Classify.RData")
+#Prediction<-as.numeric(c("NA",Prediction))
+#Prediction<-xts(Prediction,Date)
+#VixDat<-merge.xts(VixDat,Prediction)
+
+
+#mean(1-abs(VixDat[-1,"Prediction"]-VixDat[-1,"classiffication"]))
+save(BuySell,file = "DataWork/Classify.RData")
+
+return(BuySell)
 }
